1a1a60b6ec3e9e135af41b900a5dab3359dc68fd
Gubanov_Andrey <out-kuchinova-va>
2019-07-16 09:14:27 +0300

    2

--- src/main/java/ru/andgub/ClassicalMusic.java
+++ src/main/java/ru/andgub/ClassicalMusic.java
@@ -1,8 +1,28 @@
 package ru.andgub;
 
 public class ClassicalMusic implements Music {
+   private ClassicalMusic() {
+   }
+
+   public static ClassicalMusic getClassicalMusic() {
+       System.out.println("factorying...");
+       return new ClassicalMusic();
+   }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my distraction");
+    }
+
+
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
+
 }
--- src/main/java/ru/andgub/TestSpring.java
+++ src/main/java/ru/andgub/TestSpring.java
@@ -8,14 +8,32 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusicTwo = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+        System.out.println(classicalMusicTwo.getSong());
+
+        System.out.println(classicalMusic);
+        System.out.println(classicalMusicTwo);
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
